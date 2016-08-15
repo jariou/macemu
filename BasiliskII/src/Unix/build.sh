b74736078e85185ac37cba036eadbd2c4d71c197
Ricky Zhang <rickyzhang@gmail.com>
2016-08-15 14:30:30 -0400

    Update build script

--- BasiliskII/src/Unix/build.sh
+++ BasiliskII/src/Unix/build.sh
@@ -1,4 +1,4 @@
 make clean
 NO_CONFIGURE=1 ./autogen.sh
-./configure --enable-sdl-video --enable-sdl-audio --disable-vosf --enable-jit-compiler --with-x
+./configure --enable-sdl-video --enable-sdl-audio --disable-vosf --enable-jit-compiler --with-x --with-gtk
 make -j 32
